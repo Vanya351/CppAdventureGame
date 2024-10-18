ee1ddf78d486adffd06278662689f1afcea0a7f6
Vanya351 <goloborodko2005@gmail.com>
2024-10-18 19:10:55 +0300

    array procedures take values as arguments instead of pointer

--- arrayProcedures.cpp
+++ arrayProcedures.cpp
@@ -1,92 +1,92 @@
 #include "arrayProcedures.h"
 
-bool isIn(short* arr, unsigned short* size, short* value) {
-    for (short i = 0; i < *size; i++) {
-        if (arr[i] == *value) return true;
+bool isIn(short* arr, unsigned short size, short value) {
+    for (short i = 0; i < size; i++) {
+        if (arr[i] == value) return true;
     }
     return false;
 }
 
-bool isIn(unsigned short* arr, unsigned short* size, unsigned short* value) {
-    for (short i = 0; i < *size; i++) {
-        if (arr[i] == *value) return true;
+bool isIn(unsigned short* arr, unsigned short size, unsigned short value) {
+    for (short i = 0; i < size; i++) {
+        if (arr[i] == value) return true;
     }
     return false;
 }
 
-bool isIn(int* arr, unsigned short* size, int* value) {
-    for (short i = 0; i < *size; i++) {
-        if (arr[i] == *value) return true;
+bool isIn(int* arr, unsigned short size, int value) {
+    for (short i = 0; i < size; i++) {
+        if (arr[i] == value) return true;
     }
     return false;
 }
 
-bool isIn(unsigned int* arr, unsigned short* size, unsigned int* value) {
-    for (short i = 0; i < *size; i++) {
-        if (arr[i] == *value) return true;
+bool isIn(unsigned int* arr, unsigned short size, unsigned int value) {
+    for (short i = 0; i < size; i++) {
+        if (arr[i] == value) return true;
     }
     return false;
 }
 
-bool isIn(long int* arr, unsigned short* size, long int* value) {
-    for (short i = 0; i < *size; i++) {
-        if (arr[i] == *value) return true;
+bool isIn(long int* arr, unsigned short size, long int value) {
+    for (short i = 0; i < size; i++) {
+        if (arr[i] == value) return true;
     }
     return false;
 }
 
-bool isIn(unsigned long int* arr, unsigned short* size, unsigned long int* value) {
-    for (short i = 0; i < *size; i++) {
-        if (arr[i] == *value) return true;
+bool isIn(unsigned long int* arr, unsigned short size, unsigned long int value) {
+    for (short i = 0; i < size; i++) {
+        if (arr[i] == value) return true;
     }
     return false;
 }
 
-bool isIn(long long int* arr, unsigned short* size, long long int* value) {
-    for (short i = 0; i < *size; i++) {
-        if (arr[i] == *value) return true;
+bool isIn(long long int* arr, unsigned short size, long long int value) {
+    for (short i = 0; i < size; i++) {
+        if (arr[i] == value) return true;
     }
     return false;
 }
 
-bool isIn(unsigned long long int* arr, unsigned short* size, unsigned long long int* value) {
-    for (short i = 0; i < *size; i++) {
-        if (arr[i] == *value) return true;
+bool isIn(unsigned long long int* arr, unsigned short size, unsigned long long int value) {
+    for (short i = 0; i < size; i++) {
+        if (arr[i] == value) return true;
     }
     return false;
 }
 
-bool isIn(float* arr, unsigned short* size, float* value) {
-    for (short i = 0; i < *size; i++) {
-        if (arr[i] == *value) return true;
+bool isIn(float* arr, unsigned short size, float value) {
+    for (short i = 0; i < size; i++) {
+        if (arr[i] == value) return true;
     }
     return false;
 }
 
-bool isIn(double* arr, unsigned short* size, double* value) {
-    for (short i = 0; i < *size; i++) {
-        if (arr[i] == *value) return true;
+bool isIn(double* arr, unsigned short size, double value) {
+    for (short i = 0; i < size; i++) {
+        if (arr[i] == value) return true;
     }
     return false;
 }
 
-bool isIn(long double* arr, unsigned short* size, long double* value) {
-    for (short i = 0; i < *size; i++) {
-        if (arr[i] == *value) return true;
+bool isIn(long double* arr, unsigned short size, long double value) {
+    for (short i = 0; i < size; i++) {
+        if (arr[i] == value) return true;
     }
     return false;
 }
 
-bool isIn(char* arr, unsigned short* size, char* value) {
-    for (short i = 0; i < *size; i++) {
-        if (arr[i] == *value) return true;
+bool isIn(char* arr, unsigned short size, char value) {
+    for (short i = 0; i < size; i++) {
+        if (arr[i] == value) return true;
     }
     return false;
 }
 
-bool isIn(unsigned char* arr, unsigned short* size, unsigned char* value) {
-    for (short i = 0; i < *size; i++) {
-        if (arr[i] == *value) return true;
+bool isIn(unsigned char* arr, unsigned short size, unsigned char value) {
+    for (short i = 0; i < size; i++) {
+        if (arr[i] == value) return true;
     }
     return false;
 }
--- arrayProcedures.h
+++ arrayProcedures.h
@@ -1,17 +1,17 @@
 // isIn - check if there is a value in the array
 
-bool isIn(short* arr, unsigned short* size, short* value);
-bool isIn(unsigned short* arr, unsigned short* size, unsigned short* value);
-bool isIn(int* arr, unsigned short* size, int* value);
-bool isIn(unsigned int* arr, unsigned short* size, unsigned int* value);
-bool isIn(long int* arr, unsigned short* size, long int* value);
-bool isIn(unsigned long int* arr, unsigned short* size, unsigned long int* value);
-bool isIn(long long int* arr, unsigned short* size, long long int* value);
-bool isIn(unsigned long long int* arr, unsigned short* size, unsigned long long int* value);
+bool isIn(short* arr, unsigned short size, short value);
+bool isIn(unsigned short* arr, unsigned short size, unsigned short value);
+bool isIn(int* arr, unsigned short size, int value);
+bool isIn(unsigned int* arr, unsigned short size, unsigned int value);
+bool isIn(long int* arr, unsigned short size, long int value);
+bool isIn(unsigned long int* arr, unsigned short size, unsigned long int value);
+bool isIn(long long int* arr, unsigned short size, long long int value);
+bool isIn(unsigned long long int* arr, unsigned short size, unsigned long long int value);
 
-bool isIn(float* arr, unsigned short* size, float* value);
-bool isIn(double* arr, unsigned short* size, double* value);
-bool isIn(long double* arr, unsigned short* size, long double* value);
+bool isIn(float* arr, unsigned short size, float value);
+bool isIn(double* arr, unsigned short size, double value);
+bool isIn(long double* arr, unsigned short size, long double value);
 
-bool isIn(char* arr, unsigned short* size, char* value);
-bool isIn(unsigned char* arr, unsigned short* size, unsigned char* value);
+bool isIn(char* arr, unsigned short size, char value);
+bool isIn(unsigned char* arr, unsigned short size, unsigned char value);
